79420565ae150f1288c0709f46220fb710253fb9
Felix Hu <stjimmy149@live.com.au>
2021-06-02 16:45:48 +1000

    Adding My API

---
+++ src/app.js
@@ -0,0 +1,37 @@
+let restify = require('restify');
+let functions = require('./functions')
+
+const SERVERURL = '';
+
+
+let server = restify.createServer();
+
+
+server.get('/hello/:name',functions.greeting);
+server.head('/hello/:name',functions.greeting);
+
+
+server.get('/osrs',functions.osrs);
+server.head('/osrs',functions.osrs);
+
+server.get('/videos',functions.getVideos);
+server.head('/videos',functions.getVideos);
+
+server.get('/user',functions.getUser);
+server.head('/user',functions.getUser);
+
+server.get('/allUser',functions.getAllUsers);
+server.head('/allUser',functions.getAllUsers);
+
+server.get('/user/:userID',functions.findUser);
+server.head('/user/:userID',functions.findUser);
+
+server.get('/videos/:videoID',functions.getVideo);
+server.head('/videos/:videoID',functions.getVideo);
+
+
+server.listen(8080,function(){
+	console.log('%s listening at %s', server.name, server.url);
+})
+
+
---
+++ src/functions.js
@@ -0,0 +1,87 @@
+module.exports = {
+	greeting: function response(req,res,next){
+		res.send('Hello '+ req.params.name);
+		next();
+	},
+
+	osrs: function osrsStats(req,res,next){
+		res.header('token','given-token');
+		res.contentType = 'json';
+		res.send({hello: 'world'});
+		next();
+	},
+
+	getVideos: function getVideos(res,next){
+		res.header('token','given-token');
+		res.contentType = 'string'
+		let jsonObject = [
+			{videoID: 1},
+			{videoID: 2},
+		]
+		res.send(jsonObject);
+		next();
+	},
+	getVideo: function getVideo(req,res,next){
+		res.header('token','given-token');
+		res.contentType = 'json';
+		let jsonObject =
+			{
+				videoID: req.params.videoID,
+				videoName: 'testVideo',
+
+			}
+
+
+		res.send(jsonObject);
+		next();
+	},
+
+
+	getAllUsers: function getAllUsers(res,next){
+		res.header('token','given-token');
+		res.contentType = 'string'
+		let jsonObject = [
+			{userID: 1},
+			{userID: 2},
+		]
+		res.send(jsonObject);
+		next();
+	},
+
+	getUser: function getUserDetails(res,next){
+		res.header('token','given-token');
+		res.contentType = 'json';
+		let jsonObject =
+			{
+				userID: 1,
+				username:'Icekraks',
+				password: 'lol',
+				displayName: 'Icekraks',
+				instagram: 'Icekraks',
+				youtube: 'Icekraks',
+			}
+
+
+		res.send(jsonObject);
+		next();
+	},
+
+	findUser: function findUser(req,res,next){
+		res.header('token','given-token');
+		res.contentType = 'json';
+		let jsonObject =
+			{
+				userID: req.params.userID,
+				username:'Icekraks',
+				password: 'lol',
+				displayName: 'Icekraks',
+				instagram: 'Icekraks',
+				youtube: 'Icekraks',
+			}
+
+
+		res.send(jsonObject);
+		next();
+	},
+
+}
